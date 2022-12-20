8b8faa7b1f0b5f50eaf9a3485c520bc4ed949132
Oric <marcel.brandl@pm.me>
2022-12-20 19:19:10 +0100

    Idk, force stuff?

--- pages/api/instances/verify/[key].ts
+++ pages/api/instances/verify/[key].ts
@@ -1,45 +1,47 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Prisma } from '@prisma/client'
-import prismac from '../../../../lib/prisma'
+import type { NextApiRequest, NextApiResponse } from "next"
+import { Prisma } from "@prisma/client"
+import prismac from "../../../../lib/prisma"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { key } = req.query
-    const apikey = key as unknown as string
-    if (req.method !== 'GET') {
-        return res.status(405).json({ message: 'Invalid API Method'})
-    }
+  const { key } = req.query
+  const apikey = key as unknown as string
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Invalid API Method" })
+  }
 
-    const instanceData = req.body
-    
-    async function checkKey(instanceKey: string) {
-        const instanceEntry = await prismac.instances.findFirst({ where: { api_key: instanceKey }, select: { uri: true } })
-        if (instanceEntry) {
-            return instanceEntry.uri
-        } else {
-            return "failed"
-        }
-    }
-    
-    if (await checkKey(apikey) == "failed") {
-        res.status(400).json({"message": "Incorrect API Key"})
+  const instanceData = req.body
+
+  async function checkKey(instanceKey: string) {
+    const instanceEntry = await prismac.instances.findFirst({
+      where: { api_key: instanceKey },
+      select: { uri: true },
+    })
+    if (instanceEntry) {
+      return instanceEntry.uri
     } else {
-        try {
-            let check = await checkKey(apikey)
-            const savedInstance = await prismac.instances.update({
-                where: { uri: check },
-                data: { verified: true }
-            })
-            res.status(200).json({"message": "Instance added successfully"})
-        } catch (err) {
-            if (err instanceof Prisma.PrismaClientKnownRequestError){
-                if (err.code === 'P2025'){
-                    res.status(400).json({"message": "instance not in database"})
-                }else{
-                    res.status(400).json({"message": err.message })
-                }
-            }
-        }
+      return "failed"
     }
+  }
 
+  if ((await checkKey(apikey)) == "failed") {
+    res.status(400).json({ message: "Incorrect API Key" })
+  } else {
+    try {
+      let check = await checkKey(apikey)
+      const verifiedInstance = await prismac.instances.update({
+        where: { uri: check },
+        data: { verified: true },
+      })
+      res.status(200).json({ message: "Instance added successfully" })
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === "P2025") {
+          res.status(400).json({ message: "instance not in database" })
+        } else {
+          res.status(400).json({ message: err.message })
+        }
+      }
+    }
+  }
 }
