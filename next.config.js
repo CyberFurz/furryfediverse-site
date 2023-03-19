c9a232716e3450db1d019fdbea2235905c41fa78
Timothy Rogers <github@hack13.me>
2023-03-19 10:46:08 -0400

    Update next.config.js
    another quick hot-fix

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ module.exports = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  },
-  domains: [
+    domains: [
       {
         // Note: subdomains must be included in the domain value to be matched
         // e.g. www.example.com should be used if that is the expected hostname
         domain: 'furryfediverse.org',
         defaultLocale: 'en',
       }
-  ]
+    ]
+  }
 }
