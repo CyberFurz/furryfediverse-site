2b437806cbe78f93725d1adbb4b903dede95f0a7
Timothy Rogers <github@hack13.me>
2023-03-19 10:44:54 -0400

    Update next.config.js
    trying to fix issue with compile

--- next.config.js
+++ next.config.js
@@ -5,5 +5,13 @@ module.exports = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  }
+  },
+  domains: [
+      {
+        // Note: subdomains must be included in the domain value to be matched
+        // e.g. www.example.com should be used if that is the expected hostname
+        domain: 'furryfediverse.org',
+        defaultLocale: 'en',
+      }
+  ]
 }
