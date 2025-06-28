d1e9d7908a0556579a9820b25bd3078a7222d7a3
Timothy Rogers <github@hack13.me>
2025-06-28 19:19:47 -0400

    forgot next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // Enable ISR in production
+    isrMemoryCacheSize: 0,
+  },
+  // Ensure static generation works with ISR
+  output: 'standalone',
 }
 
 module.exports = nextConfig
